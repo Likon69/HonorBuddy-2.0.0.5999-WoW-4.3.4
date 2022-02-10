3071142df52733c0238a7a65d5b1eb17cf92f068
Likon69 <andrei69330@gmail.com>
2022-02-10 02:33:24 +0100

    Update SetHearthstone

--- Quest Behaviors/SetHearthstone.cs
+++ Quest Behaviors/SetHearthstone.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Threading;
-using Styx.Database;
 using Styx.Helpers;
 using Styx.Logic.BehaviorTree;
 using Styx.Logic.Inventory.Frames.Gossip;
@@ -16,283 +12,133 @@ using Action = TreeSharp.Action;
 
 namespace Styx.Bot.Quest_Behaviors
 {
-    //
-    // Class: SetHearthstone
-    // Author: Azaril
-    // Version 1.0.0
-
-    //
-    // Example usage:
-    //
-    // <CustomBehavior File="SetHearthstone" MobId="25841" X="3523.637" Y="5015.019" Z="-0.9931576" HearthLocation="123" WithinDistance="1000"/>  
-    //
-    // WithinDistance - Optional parameter to indicate only to get hearthstone if within range. If not present, then the bot will move from any distance
-    //                  to the hearthstone provider.
-    //
-
     public class SetHearthstone : CustomForcedBehavior
     {
+        private readonly string _goalText;
+        private bool _done;
+
         public SetHearthstone(Dictionary<string, string> args)
             : base(args)
         {
-            m_Complete = false;
+            NpcId = GetAttributeAsNullable<int>("MobId", false, ConstrainAs.MobId, new[] { "NpcId" }) ?? 0;
+            AreaId = GetAttributeAsNullable<int>("AreaId", false, ConstrainAs.MobId, null) ?? 0;
+            Location = GetAttributeAsNullable<WoWPoint>("", false, ConstrainAs.WoWPointNonEmpty, null) ?? Me.Location;
+            Name = GetAttributeAs<string>("Name", false, ConstrainAs.StringNonEmpty, null) ?? "";
 
-            try
+            if (!string.IsNullOrEmpty(Name))
             {
-                if (!uint.TryParse(Args["MobId"], out m_MobEntryID))
-                {
-                    throw new ArgumentException("Parsing attribute 'MobId' in SetHearthstone behavior failed! Please check your profile!");
-                }
-
-                if (Args.ContainsKey("WithinDistance"))
-                {
-                    float Distance = 0.0f;
-
-                    if (!float.TryParse(Args["WithinDistance"], out Distance))
-                    {
-                        throw new ArgumentException("Parsing attribute 'WithinDistance' in SetHearthstone behavior failed! Please check your profile!");
-                    }
-
-                    m_WithinDistance = Distance;
-                }
-                else
-                {
-                    m_WithinDistance = null;
-                }
-
-                float X;
-                float Y;
-                float Z;
-    
-                if (!float.TryParse(Args["X"], out X))
-                {
-                    throw new ArgumentException("Parsing attribute 'X' in SetHearthstone behavior failed! Please check your profile!");
-                }
-
-                if (!float.TryParse(Args["Y"], out Y))
-                {
-                    throw new ArgumentException("Parsing attribute 'Y' in SetHearthstone behavior failed! Please check your profile!");
-                }
-
-                if (!float.TryParse(Args["Z"], out Z))
-                {
-                    throw new ArgumentException("Parsing attribute 'Z' in SetHearthstone behavior failed! Please check your profile!");
-                }
-
-                m_Location = new WoWPoint(X, Y, Z);
-
-                if (!uint.TryParse(Args["HearthLocation"], out m_HearthLocation))
-                {
-                    throw new ArgumentException("Parsing attribute 'HearthLocation' in SetHearthstone behavior failed! Please check your profile!");
-                }
+                _goalText = "Setting Hearthstone at " + Name;
             }
-            catch (Exception Ex)
+            else
             {
-                Logging.Write(string.Format("Failed to create SetHearthstone behavior.\nException:\n{0}", Ex.ToString()));
-
-                Thread.CurrentThread.Abort();
+                _goalText = "Setting Hearthstone at NPC #" + NpcId;
             }
         }
 
-        protected override Composite CreateBehavior()
-        {
-            return m_Root ?? (m_Root = InternalCreateBehavior());
-        }
+        public int NpcId { get; set; }
+        public WoWPoint Location { get; set; }
+        public string Name { get; set; }
+        public int AreaId { get; set; }
+        private LocalPlayer Me { get { return (StyxWoW.Me); } }
 
-        WoWUnit GetTargetUnit()
-        {
-            WoWUnit Unit = (from WoWUnit SearchUnit in ObjectManager.GetObjectsOfType<WoWUnit>()
-                            where SearchUnit.Entry == m_MobEntryID
-                            select SearchUnit).FirstOrDefault();
+        public override bool IsDone { get { return _done; } }
 
-            return Unit;
-        }
-
-        Composite InternalCreateBehavior()
-        {
-            //
-            // Check if the behavior is not yet complete.
-            //
-            return new Decorator(TreeContext =>
-            {
-                return !m_Complete;
-            },
-                new PrioritySelector(
-                    //
-                    // While not in interact range, move to the target using the mob location.
-                    //
-                    new Decorator(TreeContext =>
-                    {
-                        WoWUnit Unit = GetTargetUnit();
-
-                        return (Unit != null) && !Unit.WithinInteractRange;
-                    },
-                        //
-                        // Move to the unit.
-                        //
-                        new Action(TreeContext =>
-                        {
-                            WoWUnit Unit = GetTargetUnit();
-
-                            return Navigator.GetRunStatusFromMoveResult(Navigator.MoveTo(Unit.Location));
-                        })
-                    ),
-                    //
-                    // While the unit is not found, move to the target location specified.
-                    //
-                    new Decorator(TreeContext =>
-                    {
-                        WoWUnit Unit = GetTargetUnit();
-
-                        return (Unit == null);
-                    },
-                        //
-                        // Move to the location.
-                        //
-                        new Action(TreeContext =>
-                        {
-                            return Navigator.GetRunStatusFromMoveResult(Navigator.MoveTo(m_Location));
-                        })
-                    ),
-                    //
-                    // Check if the unit is found and within interact range.
-                    //
-                    new Decorator(TreeContext =>
-                    {
-                        WoWUnit Unit = GetTargetUnit();
-
-                        return (Unit != null) && Unit.WithinInteractRange;
-                    },
-                        new PrioritySelector(
-                            //
-                            // Check that the player is not currently talking to the target.
-                            //
-                            new Decorator(TreeContext =>
-                            {
-                                GossipFrame Frame = GossipFrame.Instance;
-
-                                return (Frame == null || !Frame.IsVisible);
-                            },
-                                new Action(TreeContext =>
-                                {
-                                    WoWUnit Unit = GetTargetUnit();
-
-                                    Unit.Interact();
-                                })
-                            ),
-                            //
-                            // Select the bind hearthstone gossip option.
-                            //
-                            new Action(TreeContext =>
-                            {
-                                GossipFrame Frame = GossipFrame.Instance;
-
-                                if (Frame != null && Frame.IsVisible)
-                                {
-                                    if (Frame.GossipOptionEntries != null)
-                                    {
-                                        foreach (GossipEntry Entry in Frame.GossipOptionEntries)
-                                        {
-                                            if (Entry.Type == GossipEntry.GossipEntryType.Binder)
-                                            {
-                                                Frame.SelectGossipOption(Entry.Index);
-
-                                                return RunStatus.Success;
-                                            }
-                                        }
-                                    }
-                                }
-
-                                return RunStatus.Failure;
-                            })
-                        )
-                    )
-                )
-            );
-        }
-
-        bool CheckWithinDistanceToTarget()
-        {
-            if (m_WithinDistance == null)
-            {
-                return true;
-            }
-
-            LocalPlayer Player = ObjectManager.Me;
-
-            if (Player != null)
-            {
-                return Player.Location.Distance(m_Location) <= m_WithinDistance;
-            }
-
-            return false;
-        }
-
-        string GetBindLocation()
-        {
-            return Lua.GetReturnVal<string>("return GetBindLocation();", 0);
-        }
-
-        public override bool IsDone
+        private WoWUnit InnKeeper
         {
             get
             {
-                return m_Complete;
+                if (NpcId == 0)
+                {
+                    return null;
+                }
+
+                return ObjectManager.GetObjectsOfType<WoWUnit>(false, false).FirstOrDefault(u => u.IsInnkeeper && u.Entry == NpcId);
             }
         }
 
         public override void OnStart()
         {
-            if (!CheckWithinDistanceToTarget())
+            TreeRoot.GoalText = _goalText;
+            Lua.Events.AttachEvent("CONFIRM_BINDER", HandleConfirmBinder);
+        }
+
+        private bool _confirmEventFired;
+        private void HandleConfirmBinder(object sender, LuaEventArgs args)
+        {
+            Lua.DoString("ConfirmBinder(); StaticPopup_Hide('CONFIRM_BINDER')");
+            Lua.Events.DetachEvent("CONFIRM_BINDER", HandleConfirmBinder);
+            _confirmEventFired = true;
+        }
+
+        private bool GossipFrameActive()
+        {
+            return GossipFrame.Instance.IsVisible;
+        }
+
+        private bool StaticPopupActive()
+        {
+            return Lua.GetReturnVal<bool>("return StaticPopup1 and StaticPopup1:IsVisible()", 0);
+        }
+
+        private void SelectSetLocationGossipOption()
+        {
+            foreach (GossipEntry entry in GossipFrame.Instance.GossipOptionEntries)
             {
-                Logging.Write("Outside of range to set hearth location, skipping action.");
-
-                m_Complete = true;
-            }
-
-            LocalPlayer Player = ObjectManager.Me;
-
-            if (Player != null)
-            {
-                Logging.Write(string.Format("Current bind location - Name: [ {0} ] - ID: [ {1} ]", GetBindLocation(), Player.HearthstoneAreaId));
-
-                if(Player.HearthstoneAreaId == m_HearthLocation)
+                if (entry.Type == GossipEntry.GossipEntryType.Binder)
                 {
-                    Logging.Write("Hearth is already set to target area, skipping action.");
-
-                    m_Complete = true;
+                    Logging.Write("Selecting gossip option: " + entry.Text + " - #" + entry.Index);
+                    GossipFrame.Instance.SelectGossipOption(entry.Index);
                 }
             }
-
-            if (!m_Complete)
-            {
-                Lua.Events.AttachEvent("CONFIRM_BINDER", OnConfirmBinder);
-            }
         }
 
-        void OnConfirmBinder(object Sender, LuaEventArgs Args)
+        private void EnsureBindingCorrect()
         {
-            Lua.DoString("ConfirmBinder();");
+            uint areaId = StyxWoW.Me.HearthstoneAreaId;
+            uint zoneId = StyxWoW.Me.ZoneId;
 
-            Lua.DoString("StaticPopup_Hide(\"CONFIRM_BINDER\");");
-
-            Lua.Events.DetachEvent("CONFIRM_BINDER", OnConfirmBinder);
-
-            m_Complete = true;
-
-            LocalPlayer Player = ObjectManager.Me;
-
-            if (Player != null)
-            {
-                Logging.Write(string.Format("New bind location - Name: [ {0} ] - ID: [ {1} ]", GetBindLocation(), Player.HearthstoneAreaId));
-            }
+            _done = areaId == zoneId;
         }
 
-        uint m_MobEntryID;
-        Nullable<float> m_WithinDistance;
-        uint m_HearthLocation;
-        WoWPoint m_Location;
-        Composite m_Root;
-        bool m_Complete;
+        protected override Composite CreateBehavior()
+        {
+            return new PrioritySelector(
+                new Decorator(
+                    ret => AreaId != 0 && StyxWoW.Me.HearthstoneAreaId == AreaId,
+                    new Action(ret => _done = true)),
+                new Decorator(
+                    ret => InnKeeper != null,
+                    new PrioritySelector(
+                        ctx => InnKeeper,
+                // Found the Innkeeper, but its a bit far away. Get within interact distance!
+                        new Decorator(
+                            ret => ((WoWUnit)ret).Distance > 4.5f,
+                            new Action(ret => Navigator.MoveTo(((WoWUnit)ret).Location))),
+                // Now, we open up the gossip frame, and see if we can find a 'set my location' option
+                        new Sequence(
+                // First, interact.
+                            new Action(ret => ((WoWUnit)ret).Interact()),
+                // Some inn keepers offer the option to select "make this your home",
+                // while others just throw the popup at you. This should ensure both are handled!
+                            new Wait(
+                                5, ret => GossipFrameActive() || StaticPopupActive(),
+                                new PrioritySelector(
+                // If we even made it here, it means we have a window open.
+                // The next part is trivial at best. FIRST, we deal with selecting
+                // The "Make this my home" gossip option.
+                                    new Decorator(
+                                        ret => GossipFrameActive(),
+                                        new Action(ret => SelectSetLocationGossipOption())),
+                                    new Wait(
+                                        5, ret => _confirmEventFired,
+                                        new Action(ret => _done = true))
+                                    )
+                                )
+                            )
+                        )
+                    ),
+                new Action(ret => Navigator.MoveTo(Location))
+                );
+        }
     }
 }
