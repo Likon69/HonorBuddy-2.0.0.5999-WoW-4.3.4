e2fead1b0eae6dcd6560d50f7116656e739ef878
unknown <Andrei69330@gmail.com>
2022-04-01 01:07:41 +0200

    Fix TreeSharp bug.

--- Bots/VitalicBot.cs
+++ Bots/VitalicBot.cs
@@ -1,32 +1,32 @@
 using Styx;
-using Styx.CommonBot;
-using Styx.CommonBot.Profiles;
-using Styx.CommonBot.Routines;
-using Styx.TreeSharp;
+using Styx.Logic.Combat;
+using Styx.Logic.Profiles;
+using TreeSharp;
+using Action = TreeSharp.Action;
 
 namespace VitalicBot
 {
-    public class VitalicBot : BotBase
-    {
-        private Composite _root;
-        public override string Name { get { return "Vitalic Bot"; } }
-        public override Composite Root { get { return _root ?? (_root = RootBehavior); } }
-        public override PulseFlags PulseFlags { get { return PulseFlags.Objects | PulseFlags.Lua; } }
+	public class VitalicBot : BotBase
+	{
+		private Composite _root;
+		public override string Name { get { return "Vitalic Bot"; } }
+		public override Composite Root { get { return _root ?? (_root = RootBehavior); } }
+		public override PulseFlags PulseFlags { get { return PulseFlags.Objects | PulseFlags.Lua; } }
 
-        public override void Start()
-        {
-            ProfileManager.LoadEmpty();
-        }
+		public override void Start()
+		{
+			ProfileManager.LoadEmpty();
+		}
 
-        private Composite RootBehavior
-        {
-            get
-            {
-		        return new Switch<bool>(ret => StyxWoW.Me.Combat,
-                    new SwitchArgument<bool>(true, RoutineManager.Current.CombatBehavior),
-                    new SwitchArgument<bool>(false, RoutineManager.Current.PreCombatBuffBehavior)
-                );
-            }
-        }
-    }
+		private Composite RootBehavior
+		{
+			get
+			{
+				return new Switch<bool>(ret => StyxWoW.Me.Combat,
+					new SwitchArgument<bool>(true, RoutineManager.Current.CombatBehavior),
+					new SwitchArgument<bool>(false, RoutineManager.Current.PreCombatBuffBehavior)
+				);
+			}
+		}
+	}
 }
